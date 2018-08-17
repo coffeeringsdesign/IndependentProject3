bb7ba1f8182d4fdecf456b3df05db94690f7b118
Kendra <kendra@coffeeringsdesign.com>
2018-08-17 12:41:13 -0700

    initialize responses and get returns from single digits

--- js/scripts.js
+++ js/scripts.js
@@ -2,55 +2,52 @@
 var simplenumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var numberReplies = ["Beep!", "Boop!", "2", "3", "4", "5", "6", "7", "8", "9"];
 var numbers = [["Beep!",0], ["Boop!",1], ["2",2] ["3",3], ["4",4], ["5",5], ["6",6], ["7",7], ["8",8] ["9",9]];
+
+// creates a blank array for the splitRobotResponse to be placed into
 var splitRobotResponse = [];
 
+// loops through the whole input number
 var toReturn = function(inputNumber){
-  for (var i = 0; i < inputNumber; i++) {
-    if (i === 0) {
-      splitRobotResponse[i] = "Beep!";
-    } else if ()
-  }
-splitRobotResponse.push(i);
+
+
+
+  //   } else if (i % 3) {
+  //     splitRobotResponse[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+  //   } else if (i > 3 || i <= 9) {
+  //     splitRobotResponse[i] = (i);
+  //   }
+  //   return splitRobotResponse;
+  //   // console.log(inputArray);
+  // }
+
 }
 
 
-// }
-// console.log(bothNumbers);
-//
-// var translatedNumbers = function(inputString) {
-//   for (var i = 0; i < sdfssss.length; i++) {
-//
-// console.log(translatedNumbers);
-// });
-// });
-//
-//
+
 // // user interface Logic
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
-      var inputNumber = $("#inputNumber").val();
+    var inputNumber = $("#inputNumber").val();
 
-  for (var i = 0; i < inputNumber.length; i++) {
-    for (var j = 0; j <numbers.length; i++) {
+    var results = toReturn(inputNumber);
 
+    // to later be backend
+    var inputArray = inputNumber.split("");
+    for (var i = 0; i < inputArray.length; i++) {
+      splitRobotResponse.push(i);
+      if (i === 0) {
+        splitRobotResponse[i] = "Beep!";
+      } else if (i === 1) {
+        splitRobotResponse[i] = "Boop!";
+      } else if (i < 9 && i > 1) {
+        splitRobotResponse[i] = (i)
+      } else if (i === 3) {
+        splitRobotResponse[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+      }
     }
 
-  }
 
-            // numbers.forEach(function(number) {
-            //   if number[1] ==={
-            //     inputNumber -= number[1];
-
-            //   }
-            // })
-
-
-//             console.log(inputString);
-//       //       finalNumbers.push(result);
-//       //       var finalResult = finalNumbers.join(" ");
-//       //       $("#beepBoopOutput").text(finalNumbers);
-//       // });
-  // })
+    console.log(splitRobotResponse);
 });
 });
