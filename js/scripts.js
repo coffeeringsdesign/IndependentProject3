f56f239660684f340a79921324a1255cecfa5703
Kendra <kendra@coffeeringsdesign.com>
2018-08-17 11:23:39 -0700

    fix issue with website freezing due to array

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var numberReplies = ["Beep!", "Boop!", "2", "3", "4", "5", "6", "7", "8", "9"];
 var number = "";
 var bothNumbers = [];
 var finalNumbers = [];
-var numbers = ([["Beep!",0], ["Boop!",1], ["2",2 ["3",3], ["4",4], ["5",5], ["6",6], ["7",7], []"8",8 []"9"9]]);
+var numbers = [["Beep!",0], ["Boop!",1], ["2",2] ["3",3], ["4",4], ["5",5], ["6",6], ["7",7], ["8",8] ["9",9]];
 
 numbers.forEach(function(number) {
 
@@ -22,19 +22,19 @@ numbers.forEach(function(number) {
 //
 //
 // // user interface Logic
-// $(document).ready(function() {
-//   $("#inputForm").submit(function(event) {
-//     event.preventDefault();
-//       var inputNumber = parseInt($("#inputNumber").val());
-//       inputNumber = inputNumber.split("");
-// // console.log(inputNumber)
-//       inputNumber.forEach(function(inputString) {
-//             var result = translatedNumbers(inputString);
+$(document).ready(function() {
+  $("#inputForm").submit(function(event) {
+    event.preventDefault();
+      var inputNumber = parseInt($("#inputNumber").val());
+      inputNumber = inputNumber.split("");
+// console.log(inputNumber)
+      inputNumber.forEach(function(inputString) {
+            var result = translatedNumbers(inputString);
 //             console.log(inputString);
 //       //       finalNumbers.push(result);
 //       //       var finalResult = finalNumbers.join(" ");
 //       //       $("#beepBoopOutput").text(finalNumbers);
 //       // });
-//   })
-// });
-// });
+  })
+});
+});
