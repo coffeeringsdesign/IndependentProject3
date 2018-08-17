ee7cdc9b4b2c87ff9f95dc34ab465fabc023c0b3
Kendra <kendra@coffeeringsdesign.com>
2018-08-17 16:02:55 -0700

    testing matrix

--- js/scripts.js
+++ js/scripts.js
@@ -28,36 +28,52 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputNumber = $("#inputNumber").val();
-    var newInputArray = inputNumber.split("")
+    var newInputArray = inputNumber.split("").map(function(t){return parseInt(t)});
     var robotResponseArray = [];
     var beep = "Beep!"
     var boop = "Boop!"
+    var number;
     var dave = "I'm sorry, Dave. I'm afraid I can't do that."
-// .map(function(t){return parseInt(t)});
-    // var results = toReturn(inputNumber);
+    var trial = ([["Beep!",0],["Boop!",1],["2",2],["3",3],["4",4],["5",5],["6",6],["7",7],["8",8],["9",9]]);
+
+
+    trail.forEach(function(number) {
+      while (newInputArray[i] = number[1]) {
+        robotResponseArray.push(number[0]);
+    }
+  });
+});
+});
+
+//     for (var i = 0; i < newInputArray.length; i +=1) {
+//       for (var j = 0; j < trial.length; j +=1) {
+//         if (i = trail[1]) {
+//           robotResponseArray.push(trail[0]);
+//         }
+//       }
+//     }
+//   });
+// });
+
 
     // to later be backend
 
-    if (inputNumber / 3) {
-      robotResponseArray.push(dave);
-      return;
-    }
+    // if (inputNumber / 3) {
+    //   robotResponseArray.push(dave);
+    //   return;
+    // }
 
-    for (var i = 0; i < newInputArray.length; i +=1) {
-      if (i === "0") {
-        robotResponseArray.push(beep);
-      } else if (i === "1") {
-        robotResponseArray.push(boop);
-      } else if (i === "2" || i === "3" || i === "4" || i === "5" || i === "6" || i === "7" || i === "8" || i === "9") {
-        robotResponseArray.push(i);
-        console.log(robotResponseArray);
-      }
-    }
-
-    newInputArray.indexOf("0", 4);
-
-    $("#result").text(robotResponseArray);
-
-
-});
-});
+    // for (var i = 0; i < newInputArray.length; i +=1) {
+    //   if (i === "0") {
+    //     robotResponseArray.push(beep);
+    //   } else if (i === "1") {
+    //     robotResponseArray.push(boop);
+    //   } else if (i === "2" || i === "3" || i === "4" || i === "5" || i === "6" || i === "7" || i === "8" || i === "9") {
+    //     robotResponseArray.push(i);
+    //     console.log(robotResponseArray);
+    //   }
+    // }
+    //
+    // newInputArray.indexOf("0", 4);
+    //
+    // $("#result").text(robotResponseArray);
