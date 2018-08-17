0e37ecb242d662d8bd4f643cc5727330700fd343
Kendra <kendra@coffeeringsdesign.com>
2018-08-17 16:26:00 -0700

    bring beepboops back but triggering at charAt not integer

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,20 @@
 // creates a blank array for the splitRobotResponse to be placed into
 
 // loops through the whole input number
-var toReturn = function(inputNumber){
-
-
-
-  //   } else if (i % 3) {
-  //     splitRobotResponse[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
-  //   } else if (i > 3 || i <= 9) {
-  //     splitRobotResponse[i] = (i);
-  //   }
-  //   return splitRobotResponse;
-  //   // console.log(inputArray);
-  // }
-
-}
+// var toReturn = function(inputNumber){
+//
+//
+//
+//   //   } else if (i % 3) {
+//   //     splitRobotResponse[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+//   //   } else if (i > 3 || i <= 9) {
+//   //     splitRobotResponse[i] = (i);
+//   //   }
+//   //   return splitRobotResponse;
+//   //   // console.log(inputArray);
+//   // }
+//
+// }
 
 
 
@@ -27,24 +27,48 @@ $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
 
+
+    // to later be backend
+
+    if (inputNumber / 3) {
+      robotResponseArray.push(dave);
+      // return;
+    }
     var inputNumber = $("#inputNumber").val();
-    var newInputArray = inputNumber.split("").map(function(t){return parseInt(t)});
+    var newInputArray = inputNumber.split("");
     var robotResponseArray = [];
-    var beep = "Beep!"
-    var boop = "Boop!"
-    var number;
-    var dave = "I'm sorry, Dave. I'm afraid I can't do that."
-    var trial = ([["Beep!",0],["Boop!",1],["2",2],["3",3],["4",4],["5",5],["6",6],["7",7],["8",8],["9",9]]);
+    var beep = "Beep!";
+    var boop = "Boop!";
+    var dave = "I'm sorry, Dave. I'm afraid I can't do that.";
+    var robotResponseArray = [];
 
-
-    trail.forEach(function(number) {
-      while (newInputArray[i] = number[1]) {
-        robotResponseArray.push(number[0]);
+    for (var i = 0; i < newInputArray.length; i +=1) {
+      if (i === "0") {
+        robotResponseArray.push(beep);
+      } else if (i === "1") {
+        robotResponseArray.push(boop);
+      } else if (i === "2" || i === "3" || i === "4" || i === "5" || i === "6" || i === "7" || i === "8" || i === "9") {
+        robotResponseArray.push(i);
+      } else {
+        alert("this aint working");
+      }
+      console.log(newInputArray);
     }
   });
-});
-});
+  });
 
+
+  // var number;
+//     var trial = [["Beep!",0],["Boop!",1],["2",2],["3",3],["4",4],["5",5],["6",6],["7",7],["8",8],["9",9]];
+//
+// // two different attempts to use the matrix
+//     trail.forEach(function(number) {
+//       while (newInputArray[i] = number[1]) {
+//         robotResponseArray.push(number[0]);
+//     }
+//   });
+// });
+// });
 //     for (var i = 0; i < newInputArray.length; i +=1) {
 //       for (var j = 0; j < trial.length; j +=1) {
 //         if (i = trail[1]) {
@@ -55,25 +79,7 @@ $(document).ready(function() {
 //   });
 // });
 
+// .map(function(t){return parseInt(t)});
 
-    // to later be backend
 
-    // if (inputNumber / 3) {
-    //   robotResponseArray.push(dave);
-    //   return;
-    // }
-
-    // for (var i = 0; i < newInputArray.length; i +=1) {
-    //   if (i === "0") {
-    //     robotResponseArray.push(beep);
-    //   } else if (i === "1") {
-    //     robotResponseArray.push(boop);
-    //   } else if (i === "2" || i === "3" || i === "4" || i === "5" || i === "6" || i === "7" || i === "8" || i === "9") {
-    //     robotResponseArray.push(i);
-    //     console.log(robotResponseArray);
-    //   }
-    // }
-    //
-    // newInputArray.indexOf("0", 4);
-    //
     // $("#result").text(robotResponseArray);
