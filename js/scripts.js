c7768fc0bb3d4c476dc40a46359e201d3ecb4d47
Kendra <kendra@coffeeringsdesign.com>
2018-08-17 11:19:07 -0700

    add in matrix and begin js loop 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,40 @@
-// business logic
-// var nonNumbers = [NaN];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var numberReplies = ["Beep!", "Boop!", "2", "3", "4", "5", "6", "7", "8", "9"]
+// // business logic
+var simplenumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numberReplies = ["Beep!", "Boop!", "2", "3", "4", "5", "6", "7", "8", "9"];
+var number = "";
 var bothNumbers = [];
-
-for (var i = 0; i < numbers.length; i++) {
-  var number = numbers[i];
-  for (var j = 0, j < numberReplies.length; i++) {
-    var numberReply = numberReplies[j];
-    var combinedNumbers = [number, numberReply];
-    bothNumbers.push(combinedNumbers);
-}
-console.log(bothNumbers);
-
 var finalNumbers = [];
+var numbers = ([["Beep!",0], ["Boop!",1], ["2",2 ["3",3], ["4",4], ["5",5], ["6",6], ["7",7], []"8",8 []"9"9]]);
 
-var translatedNumbers = function(inputString) {
-  for (var i = 0; i < sdfssss.length; i++) {
-    if (NaN.includes(inputstring.charAt(i))) {
-      alert("Please enter a single number this time?");
+numbers.forEach(function(number) {
+
+})
+
+// }
+// console.log(bothNumbers);
+//
+// var translatedNumbers = function(inputString) {
+//   for (var i = 0; i < sdfssss.length; i++) {
+//
 // console.log(translatedNumbers);
-    }
-  }
-}
-
-
-// user interface Logic
-$(document).ready(function() {
-  $("#inputForm").submit(function(event) {
-    event.preventDefault();
-      var inputNumber = parseInt($("#inputNumber").val())
-
-      // inputNumber.forEach(function(inputString) {
-      //       var result = translatedNumbers(inputString);
-      //       finalNumbers.push(result);
-      //       var finalResult = finalNumbers.join(" ");
-      //       $("#beepBoopOutput").text(finalNumbers);
-      // });
-  });
-});
+// });
+// });
+//
+//
+// // user interface Logic
+// $(document).ready(function() {
+//   $("#inputForm").submit(function(event) {
+//     event.preventDefault();
+//       var inputNumber = parseInt($("#inputNumber").val());
+//       inputNumber = inputNumber.split("");
+// // console.log(inputNumber)
+//       inputNumber.forEach(function(inputString) {
+//             var result = translatedNumbers(inputString);
+//             console.log(inputString);
+//       //       finalNumbers.push(result);
+//       //       var finalResult = finalNumbers.join(" ");
+//       //       $("#beepBoopOutput").text(finalNumbers);
+//       // });
+//   })
+// });
+// });
