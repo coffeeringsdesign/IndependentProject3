a62966597e4d563d74873b0836c70c701b250c23
Kendra <kendra@coffeeringsdesign.com>
2018-08-17 15:27:35 -0700

    try to get beep and boop to push into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 // // business logic
-var simplenumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var numberReplies = ["Beep!", "Boop!", "2", "3", "4", "5", "6", "7", "8", "9"];
-var numbers = [["Beep!",0], ["Boop!",1], ["2",2] ["3",3], ["4",4], ["5",5], ["6",6], ["7",7], ["8",8] ["9",9]];
+
 
 // creates a blank array for the splitRobotResponse to be placed into
-var splitRobotResponse = [];
 
 // loops through the whole input number
 var toReturn = function(inputNumber){
@@ -26,28 +23,41 @@ var toReturn = function(inputNumber){
 
 // // user interface Logic
 $(document).ready(function() {
+  $('#inputForm').trigger("reset");
   $("#inputForm").submit(function(event) {
     event.preventDefault();
-    var inputNumber = $("#inputNumber").val();
 
-    var results = toReturn(inputNumber);
+    var inputNumber = $("#inputNumber").val();
+    var newInputArray = inputNumber.split("")
+    var robotResponseArray = [];
+    var beep = "Beep!"
+    var boop = "Boop!"
+    var dave = "I'm sorry, Dave. I'm afraid I can't do that."
+// .map(function(t){return parseInt(t)});
+    // var results = toReturn(inputNumber);
 
     // to later be backend
-    var inputArray = inputNumber.split("");
-    for (var i = 0; i < inputArray.length; i++) {
-      splitRobotResponse.push(i);
-      if (i === 0) {
-        splitRobotResponse[i] = "Beep!";
-      } else if (i === 1) {
-        splitRobotResponse[i] = "Boop!";
-      } else if (i < 9 && i > 1) {
-        splitRobotResponse[i] = (i)
-      } else if (i === 3) {
-        splitRobotResponse[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+
+    if (inputNumber / 3) {
+      robotResponseArray.push(dave);
+      return;
+    }
+
+    for (var i = 0; i < newInputArray.length; i +=1) {
+      if (i === "0") {
+        robotResponseArray.push(beep);
+      } else if (i === "1") {
+        robotResponseArray.push(boop);
+      } else if (i === "2" || i === "3" || i === "4" || i === "5" || i === "6" || i === "7" || i === "8" || i === "9") {
+        robotResponseArray.push(i);
+        console.log(robotResponseArray);
       }
     }
 
+    newInputArray.indexOf("0", 4);
 
-    console.log(splitRobotResponse);
+    $("#result").text(robotResponseArray);
+
+
 });
 });
