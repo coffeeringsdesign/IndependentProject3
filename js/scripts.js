98c3a830be32ad6e02af84d6c933aac2130c7c22
Kendra <kendra@coffeeringsdesign.com>
2018-08-25 10:30:12 -0700

    get the if statement for divisible by 3 to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,40 @@
 // // business logic
-var beep = "Beep!";
-var boop = "Boop!";
-var dave = "I'm sorry, Dave. I'm afraid I can't do that.";
+var inputNumber;
+var beep = "Beep!"; //variable that says beep
+var boop = "Boop!"; // variable that says boop
+var dave = "I'm sorry, Dave. I'm afraid I can't do that."; //variable that says i'm sorry
+var robotResponseArray = []; //an empty array to put the outputs too - before joining
+// var robotResponse = robotResponseArray.toString();
 
-function textReplaceNumbers(number) {
-  var robotResponseArray = [];
-  var robotResponse = robotResponseArray.toString();
-console.log(inputNumber);
-  for (var i = 0; i <= inputNumber; i +=1) {
-    if (i !=0 && i % 3 === 0) {
-      robotResponseArray.push(dave);
+
+
+
+function textReplaceNumbers(inputNumber) { //being called with inputNumber showing
+  var numberString = inputNumber.split(" ");
+  for (var i = 0; i <= numberString.length; i +=1) { //loop thru input
+    if (inputNumber !=0 && inputNumber % 3 === 0) { // divided by 3
+      robotResponseArray.push(dave); //push dave into the array
+      console.log(robotResponseArray); //working for divided by 3
     } else if (i.includes("1")) {
       robotResponseArray.push(boop);
-    } else if (i.includes("0")) {
-      robotResponseArray.push(beep);
-    } else {
-      robotResponseArray.push(i);
+    // } else if (i.includes("0")) {
+    //   robotResponseArray.push(beep);
+    // } else {
+    //   robotResponseArray.push(i);
     }
-    return robotResponse.split(",").join(", ");
+    // return robotResponse.split(",").join(", ");
 }
 }
 // // user interface Logic
 $(document).ready(function() {
-  $("form#inputForm").submit(function(event) {
-    event.preventDefault();
-    var inputNumber = $("input#visitorInput").val();
-    console.log(inputNumber);
-    $('input[type="number"], textarea').val('');
-    var result = "";
-    textReplaceNumbers(inputNumber);
+  $("form#inputForm").submit(function(event) { //pullsdata from input form
+    event.preventDefault(); //stops automatic submit function
+    inputNumber = $("input#visitorInput").val(); //sets input number for the entered value as variable ---- not string - working
+    $('input[type="number"], textarea').val(''); //resets the form
+    var result = ""; //sets an empty varible string
+    textReplaceNumbers(inputNumber); //calls the function
+
+
     var robotResponseArray = [];
     robotResponseArray.forEach(function(number) {
       $("#results").append('<li class="finalOutput">' + textReplaceNumbers(number) + '</li>');
